49397118aa8073acc0637709c774c1382e15dbe3
sunil prakash N <sunilprakash96@gmail.com>
2022-11-21 01:54:22 +0530

    Done dynamic datatable component

--- src/app/common-components/DataTable.js
+++ src/app/common-components/DataTable.js
@@ -1,88 +1,86 @@
 import React from "react";
 import { settings, download, edit, deleteIcon } from "../../assets/images";
+import Pagination from "./pagination";
+import TableActions from "./TableActions";
 
-const DataTable = ({ headers, rows }) => {
+const DataTable = ({
+  headers,
+  body,
+  onPageChange,
+  onGetLimit,
+  limit,
+  count,
+}) => {
   return (
-    <table className="table-hover">
-      <thead>
-        <tr className="thead">
-          {headers?.map((header) => (
-            <th
-              key={`${header.text}${header.key}`}
-              className={`${header.className && header.className}`}
-            >
-              {header.text}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody id="toolLists">
-        {rows?.map((row, index) => (
-          <tr key={`tr${index}`}>
-            <td>{row.toolName}</td>
-            <td>{row.type}</td>
-            <td>{row.url}</td>
-            <td className="text-center tdico">
-              <a
-                href="#"
-                onclick="callRoleConfig('24');"
-                title="Role Configuration"
+    <>
+      <TableActions onGetLimit={(limit) => onGetLimit(limit)} limit={limit} />
+      <table className="table-hover">
+        <thead>
+          <tr className="thead">
+            {headers?.map((header) => (
+              <th
+                key={`${header.text}${header.key}`}
+                className={`${header.className && header.className}`}
               >
-                <img src={settings} alt="setting" />{" "}
-              </a>
-            </td>
-            <td className="text-center tdico">
-              <a href="/entitlement/report?toolName=Nexus" title="Download">
-                <img src={download} alt="download" />
-              </a>
-            </td>
-            <td className="text-center">
-              <label className="toggle-control">
-                <input
-                  type="checkbox"
-                  className="switchInput"
-                  id="switchInput_0"
-                  onchange="changeToolStatus('24', '0');"
-                  href="javascript:void(0);"
-                />
-                <span className="control"></span>
-              </label>
-            </td>
-            <td className="table_icon">
-              <table className="action">
-                <tbody>
-                  <tr>
-                    <td>
-                      <a
-                        data-bs-toggle="offcanvas"
-                        href="#addnewtool-popup"
-                        role="button"
-                        aria-controls="addnewtool-popup"
-                        onclick="getToolData('24')"
-                        title="Edit"
-                      >
-                        <img src={edit} alt="Edit" />
-                      </a>
-                    </td>
-                    <td>
-                      <a
-                        onclick="beforedelete('24')"
-                        title="Delete"
-                        className="delete_cmdb"
-                        data-bs-toggle="modal"
-                        data-bs-target="#deletepopup"
-                      >
-                        <img src={deleteIcon} alt="Delete" />
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
+                {header.text}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody id="toolLists">
+          {body?.map((row, index) => (
+            <tr key={`tr${index}`}>
+              {headers.map((header, index) =>
+                header?.component ? (
+                  <td className="text-center tdico">
+                    {header?.component(row)}
+                  </td>
+                ) : header.value === "action" ? (
+                  <td className="table_icon">
+                    <table className="action">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <a
+                              data-bs-toggle="offcanvas"
+                              href="#addnewtool-popup"
+                              role="button"
+                              aria-controls="addnewtool-popup"
+                              onclick="getToolData('24')"
+                              title="Edit"
+                            >
+                              <img src={edit} alt="Edit" />
+                            </a>
+                          </td>
+                          <td>
+                            <a
+                              onclick="beforedelete('24')"
+                              title="Delete"
+                              className="delete_cmdb"
+                              data-bs-toggle="modal"
+                              data-bs-target="#deletepopup"
+                            >
+                              <img src={deleteIcon} alt="Delete" />
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                ) : (
+                  <td>{row[header.value]}</td>
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        rows={body}
+        onPageChange={(page) => onPageChange(page)}
+        totalPages={Math.ceil(count / limit)}
+      />
+    </>
   );
 };
 export default DataTable;
--- src/app/services/configurationApi.js
+++ src/app/services/configurationApi.js
@@ -5,13 +5,13 @@ const MODULE_BASE_URL = "/api/v1";
 
 export const configurationApi = api.injectEndpoints({
   endpoints: (build) => ({
-    getToolsList: build.query({
-      query: (page = 0, length = 10) =>
-        `tools/listByItemsPerPage/${page}/${length}`,
+    getToolsList: build.mutation({
+      query: ({page = 0, limit = 10}) => {
+        return `http://localhost:7111/api/v1/tools/listByItemsPerPage/${page}/${limit}`;
+      },
     }),
-    getGroupsList: build.query({
-      query: (page = 0, length = 10) =>
-        `groups/listByItemsPerPage/${page}/${length}`,
+    getToolsListCount: build.query({
+      query: () => `http://localhost:7111/api/v1/tools/count`,
     }),
   }),
   overrideExisting: true,
