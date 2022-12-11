8f1a9dba62de4f58065b246aa37fa768a6739456
murugan <murugan.n.test@gmail.com>
2022-12-11 17:31:03 +0530

    focus search input of select on mount to improve ux

--- src/app/common-components/select/Control.js
+++ src/app/common-components/select/Control.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { components } from "react-select";
 
+
 const Control = (props) => {
-    return (
-        <components.Control {...props} className="control" />
-    )
-}
+  const selectControlRef = useRef(null);
+  return (
+    <div onClick={() => props.selectProps.toggleMenu()}>
+      <components.Control {...props} className="control" />
+    </div>
+  );
+};
 
 export default Control;
--- src/app/common-components/select/index.js
+++ src/app/common-components/select/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import Select from "react-select";
 import Option from "./Option";
 import "./select.styles.scss";
@@ -7,10 +7,14 @@ import ValueContainer from "./ValueContainer";
 import Menu from "./Menu";
 import MenuList from "./MenuList";
 import DropdownIndicator from "./DropdownIndicator";
-import CreatableSelect from"react-select/creatable";
+import IndicatorsContainer from "./IndicatorsContainer";
+import CreatableSelect from "react-select/creatable";
+import { get } from "lodash";
 
 const CustomSelect = (props) => {
   const [filteredOptions, setFilteredOptions] = useState([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     setFilteredOptions(props?.options.length ? props.options : []);
   }, [props.options]);
@@ -19,31 +23,49 @@ const CustomSelect = (props) => {
     if (props.isCheckboxSelect) {
       components = {
         Control,
+        IndicatorsContainer,
         IndicatorSeparator: () => null,
         ValueContainer,
         Menu,
         MenuList,
         Option,
-        DropdownIndicator
+        DropdownIndicator,
       };
     }
     return components;
   };
   const getFilteredOptions = (searchText) => {
-    setFilteredOptions(props?.options?.filter(option => option.label.toLowerCase().includes(searchText.toLowerCase())))
-  }
+    setFilteredOptions(
+      props?.options?.filter((option) =>
+        option.label.toLowerCase().includes(searchText.toLowerCase())
+      )
+    );
+  };
+
+  const toggleMenu = useCallback((status) => {
+    if(status) {
+      setIsMenuOpen(status);
+    } else {
+      setIsMenuOpen((state) => {
+        return !state
+      });
+    }
+  }, [isMenuOpen]);
+
   return (
     <Select
       {...props}
       components={getCustomComponents()} //Overrided reac-select components to match our theme style
       className="custom-select" //wrapper class name to have css parent selector for scoped styling
-      isClearable={false} 
+      isClearable={false}
       // noOptionsMessage={props.noOptionsMessage ? props.noOptionsMessage : "No results"}
       openMenuOnClick={true}
       backspaceRemovesValue={false} //keep this false for search input to get enabled otherwise keyboard events are overrided by react-select
       isSearchable={true}
       options={filteredOptions}
       filterOptionsBySearch={getFilteredOptions}
+      menuIsOpen={isMenuOpen}
+      toggleMenu={toggleMenu}
     />
   );
 };
